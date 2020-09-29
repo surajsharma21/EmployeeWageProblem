a70e8467f9c83b003eda7c01abdd9da69f9c4e8b
Suraj <surajshrm000@gmail.com>
2020-09-29 13:24:52 +0530

    [Suraj] Add: Check if Employee is Present or Absent

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -6,7 +6,17 @@ namespace EmployeeWageProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Problem");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
