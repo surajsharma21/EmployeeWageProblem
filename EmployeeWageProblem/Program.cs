aac2dab48c2712a0f92d9fac81dcd953f89ea55a
Suraj <surajshrm000@gmail.com>
2020-09-29 13:18:59 +0530

    [Suraj] Add: Welcome Message

---
+++ EmployeeWageProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Problem");
+        }
+    }
+}
