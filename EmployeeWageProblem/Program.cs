f4e247f05aa77847a134ad0f71c51c2deb5328ed
Suraj <surajshrm000@gmail.com>
2020-09-29 13:44:03 +0530

    [Suraj] Add: Calculating PartTime Wages

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -8,14 +8,19 @@ namespace EmployeeWageProblem
         {
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
             if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
             }
             else
             {
